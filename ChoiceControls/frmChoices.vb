49daec435540b0055a1c27cf6a75bf24ec2e75c2
NMene018 <nmene018@fiu.edu>
2020-01-22 17:46:19 -0500

    Add project files.

---
+++ ChoiceControls/frmChoices.vb
@@ -0,0 +1,20 @@
+﻿Public Class frmChoices
+
+    Private Sub LoadComboBoxChoices()
+        cboSaleItems.Items.Add("Long sleeve shirt")
+        cboSaleItems.Items.Add("Short sleeve shirt")
+        cboSaleItems.Items.Add("Lightweight jacket")
+    End Sub
+
+    Private Sub frmChoices_Load(sender As Object, e As EventArgs) Handles Me.Load
+        LoadComboBoxChoices()
+    End Sub
+
+    Private Sub cboSaleItems_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboSaleItems.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
